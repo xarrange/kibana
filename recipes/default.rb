d2fedcecc68095ab9f82cad9fc4e6f8091db82ac
xarrange <x_arrange@mail.ru>
2014-09-17 14:57:42 +0000

    working

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,24 +1,29 @@
+bash "kibana_download" do
+    code <<-EOH
+    wget #{node['kibana']['source']}
+    tar -zxf "#{node['kibana']['file']}.tar.gz"
+    mv "#{node['kibana']['file']}" "./#{node['kibana']['basedir']}/"
+    rm -rf "#{node['kibana']['file']}.tar.gz"
+    EOH
+end
+
 include_recipe "apache2"
 
-bash "kibana_download" do
-	wget node['kibana']['source']
-	tar -zxf "#{node['kibana']['file']}.tar.gz"
-	mv "#{node['kibana']['file']}" node['kibana']['basedir']
-	rm -rf "#{node['kibana']['file']}.tar.gz"
-end
+file "#{node['apache']['dir']}/sites-enabled/kibana.conf" do
+    content <<-EOH
+    Listen 8080
+    <VirtualHost *:8080>
+    DocumentRoot /opt/kibana-3.1.0
 
-file "kibana_apache_conf" do
-	path "#{node['apache']['dir']/kibana.conf"
-	content "
-	<VirtualHost *:#{node['kibana']['port']}>
-	DocumentRoot #{node['kibana']['basedir']}/#{node['kibana']['file']}
-	</VirtualHost>"
-
-	mode '640'
-	action :create
-end
-
-service "apache" do
-  supports :restart => true, :reload => true
-  action :enable
+    <Directory /opt/kibana-3.1.0>
+	AllowOverride All
+	Order allow,deny
+        Allow from all
+	Require all granted
+    </Directory>
+    </VirtualHost>
+    EOH
+    mode '640'
+    action :create
+    notifies :restart, "service[apache2]"
 end
